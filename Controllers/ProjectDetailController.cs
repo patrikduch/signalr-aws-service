99ba3f50d0f0fa972bd9d3ca304589f82fa9d045
patrikduch <duchpatrik@icloud.com>
2021-03-10 16:42:56 +0100

    Initial commit

---
+++ Controllers/ProjectDetailController.cs
@@ -0,0 +1,23 @@
+﻿// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace NetCore_Dockerization.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using NetCore_Dockerization.Models;
+
+    [Route("")]
+    [ApiController]
+    public class ProjectDetailController : ControllerBase
+    {
+        // GET: api/project
+        [HttpGet]
+        public ProjectDetail GetProjectDetail()
+        {
+            return new ProjectDetail
+            {
+                Id = 1,
+                Name = "SignalR application in .NETCore on AWS cloud platform!",
+                Author = "Patrik Duch"
+            };
+        }
+    }
+}
