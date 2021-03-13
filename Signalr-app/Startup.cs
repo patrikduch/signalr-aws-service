6e84580ede32dd1af85250b46a4848aaf14432ec
patrikduch <duchpatrik@icloud.com>
2021-03-13 10:19:38 +0100

    NetCore: CORS policy fix

--- Signalr-app/Startup.cs
+++ Signalr-app/Startup.cs
@@ -19,7 +19,14 @@ namespace NetCore_Dockerization
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder
+                    .WithOrigins("http://localhost:80")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
             services.AddControllers();
             services.AddSignalR();
         }
@@ -35,13 +42,9 @@ namespace NetCore_Dockerization
             //app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            //app.UseAuthorization();
-
-            // Make sure you call this before calling app.UseMvc()
-            app.UseCors(
-                options => options.WithOrigins("http://localhost:80").AllowAnyMethod()
-            );
+            app.UseAuthorization();
+            app.UseCors("CorsPolicy");
+           
 
             app.UseEndpoints(endpoints =>
             {
