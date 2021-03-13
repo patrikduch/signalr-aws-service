34de774d83aaaffd9be7a5ce36857d8c7a14123c
patrikduch <duchpatrik@icloud.com>
2021-03-13 14:39:53 +0100

    SignalR: New structure of signalr functionality

---
+++ Signalr-app/SignalR/handlers/UserHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace NetCore_Dockerization.SignalR.handlers
+{
+    public static class UserHandler
+    {
+        public static HashSet<string> ConnectedIds = new HashSet<string>();
+
+    }
+}
---
+++ Signalr-app/SignalR/hubs/ViewHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using NetCore_Dockerization.SignalR.handlers;
+using System;
+using System.Threading.Tasks;
+
+namespace NetCore_Dockerization.Hubs
+{
+    public class ViewHub : Hub
+    {
+        public static int ViewCount { get; set; } = 0;
+
+        public override Task OnConnectedAsync()
+        {
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task NotifyWatching()
+        {
+            ViewCount = UserHandler.ConnectedIds.Count;
+
+            // notify EVERYONE about new view count
+            await Clients.All.SendAsync("viewCountUpdate", ViewCount);
+        }
+    }
+}
